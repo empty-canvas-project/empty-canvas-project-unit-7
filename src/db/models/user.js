372c210feee13c2b9e8a041106ca7192ca66ede4
benspector-mls <ben@marcylabschool.org>
2023-04-27 15:44:36 -0400

    organizing routes and adding error handling for model

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -11,42 +11,72 @@ class User {
   }
 
   static async list() {
-    const query = 'SELECT * FROM users';
-    const { rows } = await knex.raw(query);
-    return rows.map((user) => new User(user));
+    try {
+      const query = 'SELECT * FROM users';
+      const { rows } = await knex.raw(query);
+      return rows.map((user) => new User(user));
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }
 
   static async find(id) {
-    const query = 'SELECT * FROM users WHERE id = ?';
-    const { rows: [user] } = await knex.raw(query, [id]);
-    return user ? new User(user) : null;
+    try {
+      const query = 'SELECT * FROM users WHERE id = ?';
+      const { rows: [user] } = await knex.raw(query, [id]);
+      return user ? new User(user) : null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }
 
   static async findByUsername(username) {
-    const query = 'SELECT * FROM users WHERE username = ?';
-    const { rows: [user] } = await knex.raw(query, [username]);
-    return user ? new User(user) : null;
+    try {
+      const query = 'SELECT * FROM users WHERE username = ?';
+      const { rows: [user] } = await knex.raw(query, [username]);
+      return user ? new User(user) : null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }
 
   static async create(username, password) {
-    const passwordHash = await hashPassword(password);
+    try {
+      const passwordHash = await hashPassword(password);
 
-    const query = `INSERT INTO users (username, password_hash)
-      VALUES (?, ?) RETURNING *`;
-    const { rows: [user] } = await knex.raw(query, [username, passwordHash]);
-    return new User(user);
+      const query = `INSERT INTO users (username, password_hash)
+        VALUES (?, ?) RETURNING *`;
+      const { rows: [user] } = await knex.raw(query, [username, passwordHash]);
+      return new User(user);
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }
 
   static async deleteAll() {
-    return knex.raw('TRUNCATE users;');
+    try {
+      return knex.raw('TRUNCATE users;');
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   }
 
   update = async (username) => { // dynamic queries are easier if you add more properties
-    const [updatedUser] = await knex('users')
-      .where({ id: this.id })
-      .update({ username })
-      .returning('*');
-    return updatedUser ? new User(updatedUser) : null;
+    try {
+      const [updatedUser] = await knex('users')
+        .where({ id: this.id })
+        .update({ username })
+        .returning('*');
+      return updatedUser ? new User(updatedUser) : null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
   };
 
   isValidPassword = async (password) => (
--- src/routes.js
+++ src/routes.js
@@ -6,20 +6,31 @@ const checkAuthentication = require('./middleware/check-authentication');
 const Router = express.Router();
 Router.use(addModels);
 
+Router.get('/cookieCounter', (req, res) => {
+  const { session } = req;
+  console.log(session);
+  session.viewCount = (session.viewCount || 0) + 1;
+  console.log(session.viewCount);
+  res.status(200).send({ count: session.viewCount });
+});
 
-
-Router.get('/users', userController.list);
+// Create
 Router.post('/users', userController.create);
-Router.get('/users/:id', userController.show);
-// We can use middleware slotted in between the route and the controller as well
-Router.patch('/users/:id', checkAuthentication, userController.update);
-
 Router.post('/users/login', userController.login);
-Router.delete('/users/logout', userController.logout);
-Router.get('/me', userController.showMe);
 
+// Read
+Router.get('/users', userController.list);
+Router.get('/users/:id', userController.show);
+Router.get('/me', userController.showMe);
+// checkAuthentication middleware is applied to only to this route (and /logged-in-secret)
 Router.get('/logged-in-secret', checkAuthentication, (req, res) => {
   res.send({ msg: 'The secret is: there is no secret.' });
 });
+
+// Update
+Router.patch('/users/:id', checkAuthentication, userController.update);
+
+// Delete
+Router.delete('/users/logout', userController.logout);
 
 module.exports = Router;

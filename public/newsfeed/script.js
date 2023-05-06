e8d0cd8e866a4cfd5345b1fe3dbf78765a364416
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-05-05 21:32:18 -0400

    switching

--- public/newsfeed/script.js
+++ public/newsfeed/script.js
@@ -70,3 +70,8 @@ searchButton.addEventListener("click", async (event) => {
     resultsContainer.appendChild(songDiv);
   });
 });
+
+
+document.getElementById("nav-button").addEventListener("click", function() {
+  window.location.href = "http://127.0.0.1:5500/public/createpost/createpost.html";
+});

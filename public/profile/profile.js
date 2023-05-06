c5865bf83ec27556ca619bba0f293dfd33940a30
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-05-05 23:15:51 -0400

    profile buttons connected

---
+++ public/profile/profile.js
@@ -0,0 +1,8 @@
+function openNewsfeed() {
+    window.location.href = "http://127.0.0.1:5500/public/newsfeed/index.html";
+  }
+  
+  function openCreatePost() {
+    window.location.href = "http://127.0.0.1:5500/public/createpost/createpost.html";
+  }
+  

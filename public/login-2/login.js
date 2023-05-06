7d9157bbdd8e4dcd2e51f9e5bf6756ce78162d1b
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-05-05 21:43:07 -0400

    button connecting to pages complete

---
+++ public/login-2/login.js
@@ -0,0 +1,5 @@
+const signupBtn = document.getElementById("signup-btn");
+
+signupBtn.addEventListener("click", function() {
+  window.location.href = "http://127.0.0.1:5500/public/signup/create.html";
+});
